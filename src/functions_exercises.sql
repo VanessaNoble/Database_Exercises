d59a474fdc4c5376de40dc2d9188e55357e93cfd
vanessamnoble <vanessamnoble@icloud.com>
2017-01-25 16:35:50 -0600

    part four of functions ex

--- src/functions_exercises.sql
+++ src/functions_exercises.sql
@@ -27,6 +27,19 @@ WHERE birth_date LIKE '%12-25'
       AND hire_date BETWEEN '1999-01-01' and '1999-12-31';
 
 
+# GROUP BY clause to your query for last names with 'q' and
+# not 'qu' to find the distinct combination of first and last names
+# Add a count() to your results and use ORDER BY to make it easier
+# to find employees whose unusual name is shared with others.
+#part four
+
+SELECT concat(first_name, " ",last_name) as "full_name", count(first_name) as "number of people with this name"
+from  employees
+WHERE last_name like '%q%'
+      and last_name not like '%qu%'
+group by full_name
+order by 'number of people with this name' DESC;
+
 
 
 
@@ -35,30 +48,30 @@ WHERE birth_date LIKE '%12-25'
 
 
 # notes
-
-SELECT count (DISTINCT first_name)
-from employees;
-
-# SELECT concat(first_name, " ", last_name) as "full name"
-# from employees
-# where hire_date like "%12-25";
-
-SELECT curdate();
-
-SELECT avg(salary)
-from salaries;
-
-SELECT avg(salary)
-from salaries
-where to_date > curdate();
-
-SELECT avg(salary) as "current average salary"
-FROM salaries
-where to_date > current_date();
-
-SELECT now();
-
-SELECT concat(
-    'Teaching people to code ',
-    UNIX_TIMESTAMP() - UNIX_TIMESTAMP('2014-02-04'),
-    ' seconds');
+#
+# SELECT count (DISTINCT first_name)
+# from employees;
+#
+# # SELECT concat(first_name, " ", last_name) as "full name"
+# # from employees
+# # where hire_date like "%12-25";
+#
+# SELECT curdate();
+#
+# SELECT avg(salary)
+# from salaries;
+#
+# SELECT avg(salary)
+# from salaries
+# where to_date > curdate();
+#
+# SELECT avg(salary) as "current average salary"
+# FROM salaries
+# where to_date > current_date();
+#
+# SELECT now();
+#
+# SELECT concat(
+#     'Teaching people to code ',
+#     UNIX_TIMESTAMP() - UNIX_TIMESTAMP('2014-02-04'),
+#     ' seconds');
