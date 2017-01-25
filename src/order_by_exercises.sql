08c897611ae08ade992e665c85722b9e625bcdf5
vanessamnoble <vanessamnoble@icloud.com>
2017-01-25 14:37:41 -0600

    modify (v.3) first query to order by first name

--- src/order_by_exercises.sql
+++ src/order_by_exercises.sql
@@ -4,6 +4,11 @@ WHERE first_name in ('Irena', 'Vidya', 'Maya')
   AND gender = 'M'
 ORDER BY first_name,last_name;
 
+# (v.3)Modify your first query to order by first name.
+SELECT * FROM employees
+WHERE first_name in ('Irena', 'Vidya', 'Maya')
+      AND gender = 'M'
+ORDER BY first_name,last_name;
 
 
 
