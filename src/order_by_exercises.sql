16dc805331dbf47c48c9bb50727280776b4d7de9
vanessamnoble <vanessamnoble@icloud.com>
2017-01-25 14:39:10 -0600

    (v.3)update the query to sort first name and then last name

--- src/order_by_exercises.sql
+++ src/order_by_exercises.sql
@@ -10,6 +10,12 @@ WHERE first_name in ('Irena', 'Vidya', 'Maya')
       AND gender = 'M'
 ORDER BY first_name,last_name;
 
+# Update the query to order by first name and then last name.
+SELECT *FROM employees
+WHERE first_name in ('Irena', 'Vidya', 'Maya')
+      AND gender = "M"
+ORDER BY  last_name, first_name ASC;
+
 
 
 
