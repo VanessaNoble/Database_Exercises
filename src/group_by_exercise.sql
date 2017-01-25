948d0906daab6cc0a5b5aeeaf9065fc7024b684d
vanessamnoble <vanessamnoble@icloud.com>
2017-01-25 15:26:27 -0600

    find query for employees whos last name start and end with 'E'

--- src/group_by_exercise.sql
+++ src/group_by_exercise.sql
@@ -2,4 +2,11 @@
 # use DISTINCT to find the unique titles in the titles table
 SELECT DISTINCT title
 FROM titles;
+#Update the previous query to sort the results alphabetically.
+
+# Find your query for employees whose last names start and end with 'E'.
+Select last_name
+FROM employees
+where last_name like 'e%e'
+GROUP BY last_name;
 
